8fdd4ae277cf3bfcf7a0123663dcde2eb5790c77
Tim Goyer <tgoyer@users.noreply.github.com>
2015-07-16 16:47:25 -0500

    Update master.sql

--- Spiffdog.AspNet.Identity.Dapper/master.sql
+++ Spiffdog.AspNet.Identity.Dapper/master.sql
@@ -6,33 +6,37 @@ GO
 --drop table UserLogins
 --drop table UserRoles
 --drop table Users
+--drop schema [identity]
 
-CREATE TABLE [dbo].UserRoles (
+CREATE SCHEMA [identity]
+GO
+
+CREATE TABLE [identity].UserRoles (
     [Id]	 INT IDENTITY (1 ,1) NOT NULL,
     [UserId] NVARCHAR (128) NOT NULL,
     [RoleId] INT NOT NULL
 );
 
-CREATE TABLE [dbo].UserClaims (
+CREATE TABLE [identity].UserClaims (
     [Id]         INT IDENTITY (1, 1) NOT NULL,
     [UserId]     NVARCHAR (128) NOT NULL,
     [ClaimType]  NVARCHAR (MAX) NULL,
     [ClaimValue] NVARCHAR (MAX) NULL
 );
 
-CREATE TABLE [dbo].UserLogins (
+CREATE TABLE [identity].UserLogins (
     [Id]			INT	IDENTITY (1, 1)  NOT NULL,
     [UserId]        NVARCHAR (128) NOT NULL,
     [LoginProvider] NVARCHAR (128) NOT NULL,
     [ProviderKey]   NVARCHAR (128) NOT NULL
 );
 
-CREATE TABLE [dbo].Roles (
+CREATE TABLE [identity].Roles (
     [Id]   NVARCHAR (128) NOT NULL,
     [Name] NVARCHAR (256) NOT NULL
 );
 
-CREATE TABLE [dbo].Users (
+CREATE TABLE [identity].Users (
     [Id]                   NVARCHAR (128) NOT NULL,
     [Email]                NVARCHAR (256) NULL,
     [EmailConfirmed]       BIT            NOT NULL,
